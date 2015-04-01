f80b30e65c4f7f3b57f377068dc6bce878f884e5
eugenol <eug@live.co.za>
2015-04-01 23:16:25 +0200

    change resolution

--- AllegroTest/main.cpp
+++ AllegroTest/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
 
 int main(int argc, char **argv)
 {
@@ -10,13 +11,15 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	display = al_create_display(640, 480);
+	display = al_create_display(800, 600);
 	if (!display) {
 		fprintf(stderr, "failed to create display!\n");
 		return -1;
 	}
 
 	al_clear_to_color(al_map_rgb(0, 0, 0));
+
+	//al_draw_filled_circle(320, 240, 10, al_map_rgb(255, 255, 255));
 
 	al_flip_display();
 
