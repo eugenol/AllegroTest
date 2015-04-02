347b5f102c85c885d09469580c720a03873f24ec
eugenol <eug@live.co.za>
2015-04-02 10:52:14 +0200

    use native dialog boxes for errors

--- AllegroTest/main.cpp
+++ AllegroTest/main.cpp
@@ -1,27 +1,46 @@
-#include <stdio.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_native_dialog.h>
 #include <allegro5/allegro_primitives.h>
 
 int main(int argc, char **argv)
 {
+	//Consts
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 600;
+
+	//Intitializations
+	
+	//Intitalize allegro
+	if (!al_init())
+	{
+		al_show_native_message_box(al_get_current_display(), "Error", "Error", "Allegro failed to initialize",
+			NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return -1;
+	}
+
+	//Create Display
 	ALLEGRO_DISPLAY *display = NULL;
-
-	if (!al_init()) {
-		fprintf(stderr, "failed to initialize allegro!\n");
+	display = al_create_display(SCREEN_WIDTH, SCREEN_HEIGHT);
+	if (!display)
+	{
+		al_show_native_message_box(al_get_current_display(), "Error", "Error", "Allegro failed to create the display",
+			NULL, ALLEGRO_MESSAGEBOX_ERROR);
 		return -1;
 	}
 
-	display = al_create_display(800, 600);
-	if (!display) {
-		fprintf(stderr, "failed to create display!\n");
-		return -1;
-	}
+	//Load addons
 
 	al_clear_to_color(al_map_rgb(0, 0, 0));
 
 	//al_draw_filled_circle(320, 240, 10, al_map_rgb(255, 255, 255));
 
 	al_flip_display();
+
+	al_show_native_message_box(al_get_current_display(),
+		"Window Title",
+		"Content Title",
+		"The error message here",
+		NULL, ALLEGRO_MESSAGEBOX_ERROR);
 
 	al_rest(10.0);
 
--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Settings found here:
 https://wiki.allegro.cc/index.php?title=Windows,_Visual_Studio_2010_and_Allegro_5
 
 
-make sure to set this in environment
+make sure to set this in environment in debugging, for the debuf version
 
  PATH=c:\allegro\bin;%PATH%
