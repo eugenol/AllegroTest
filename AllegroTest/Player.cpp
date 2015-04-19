59370c00c765b98049a3fbd09c27c1b0a6a6853f
eugenol <eug@live.co.za>
2015-04-19 20:00:39 +0200

    move player spesific code to player.cpp from GameObject.cpp

--- AllegroTest/Player.cpp
+++ AllegroTest/Player.cpp
@@ -20,6 +20,47 @@ void Player::setImage(ALLEGRO_BITMAP *image)
 
 void Player::Update(const bool *key)
 {
+	this->set_y_direction(0);
+	this->set_x_direction(0);
+	float x, y, velx, vely;
+	int dirx, diry;
+
+	x = this->get_x();
+	y = this->get_y();
+	velx = this->get_x_velocity();
+	vely = this->get_y_velocity();
+	dirx = this->get_x_direction();
+	diry = this->get_y_direction();
+
+	//update position
+	if (key[KEY_UP])
+	{
+		y -= vely;
+		diry = 1;
+	}
+	if (key[KEY_DOWN])
+	{
+		y += vely;
+		diry = -1;
+	}
+
+	if (key[KEY_LEFT])
+	{
+		x -= velx;
+		dirx = -1;
+	}
+
+	if (key[KEY_RIGHT])
+	{
+		x += velx;
+		dirx = +1;
+	}
+
+	this->set_x(x);
+	this->set_y(y);
+	this->set_x_direction(dirx);
+	this->set_y_direction(diry);
+
 	GameObject::Update(key);
 	sprite->Update();
 }
