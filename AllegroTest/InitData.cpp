f90d760ed340f57201bd2c54e012f45ea79349de
eugenol <eug@live.co.za>
2015-05-04 18:13:36 +0200

    fix in InitData.cpp

--- AllegroTest/InitData.cpp
+++ AllegroTest/InitData.cpp
@@ -3,7 +3,7 @@
 
 InitData::InitData() : display(NULL), event_queue(NULL), timer(NULL), player_image(NULL), enemy_image(NULL),
 						background(NULL), map(NULL), bg_music(NULL), bgInstance(NULL), laser_sound(NULL), laser_sound_instance(NULL),
-						cursorImage(NULL), cursor(NULL), player(NULL), done_loading(NULL)
+						cursorImage(NULL), cursor(NULL), player(NULL), done_loading(false)
 {
 
 }
